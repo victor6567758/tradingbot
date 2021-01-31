1a6c06c74ea790754f3838968588b1391f36095d
Victor Zoubok <victorofff@gmail.com>
2021-01-31 14:44:04 -0800

    WIP

--- tradingbot-app/src/main/java/com/tradebot/controller/rest/TradingController.java
+++ tradingbot-app/src/main/java/com/tradebot/controller/rest/TradingController.java
@@ -8,9 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.joda.time.DateTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController("/signal")
+@RequestMapping("/signal")
 @RequiredArgsConstructor
 public class TradingController {
 
