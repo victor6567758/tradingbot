e62cd0233d147aa0797bef3eabfecd65bc4be1fa
Victor Zoubok <victorofff@gmail.com>
2021-02-20 10:58:37 -0800

    WIP

---
+++ tradingbot-app/docker/standard/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+echo "The application will start in ${APP_SLEEP}s..." && sleep ${APP_SLEEP}
+exec java ${JAVA_OPTS} \
+    -Djava.security.egd=file:/dev/./urandom \
+    -DPROD_LOG_PATH=${PROD_REAL_LOG_PATH} \
+    -DPROD_LOG_LEVEL=${PROD_REAL_LOG_LEVEL} \
+    -jar "${HOME}/tradingbot-app-0.0.1-SNAPSHOT.jar" ${RUN_ARGS} "$@"
