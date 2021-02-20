b0cfe7162659ad798b80edb60136218872d7e982
Victor Zoubok <victorofff@gmail.com>
2021-02-20 12:36:54 -0800

    WIP

--- tradingbot-app/docker/standard/entrypoint.sh
+++ tradingbot-app/docker/standard/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-echo "The application will start in ${APP_SLEEP}s..." && sleep ${APP_SLEEP}
+echo "The standard application will start in ${APP_SLEEP}s..." && sleep ${APP_SLEEP}
 exec java ${JAVA_OPTS} \
     -Djava.security.egd=file:/dev/./urandom \
     -DPROD_LOG_PATH=${PROD_REAL_LOG_PATH} \
--- tradingbot-app/prepare-docker-std.sh
+++ tradingbot-app/prepare-docker-std.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker login
-docker build -t trading-bot-2.0 .
+docker build -t trading-bot-2.0 -f docker/standard/Dockerfile .
 docker tag trading-bot-2.0 dockerzzzzzzzzzzzz123/trading-bot-2.0:latest
 docker push dockerzzzzzzzzzzzz123/trading-bot-2.0:latest
