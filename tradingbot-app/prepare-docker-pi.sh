505f93c432c1db0313de0ac5a0e7ce0cb1d5ae42
Victor Zoubok <victorofff@gmail.com>
2021-02-20 07:31:19 -0800

    WIP

--- tradingbot-app/prepare-docker-pi.sh
+++ tradingbot-app/prepare-docker-pi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker login
-docker build -t trading-bot-2.0 .
+docker build -t trading-bot-2.0-pi .
 docker tag trading-bot-2.0 dockerzzzzzzzzzzzz123/trading-bot-2.0-pi:latest
 docker push dockerzzzzzzzzzzzz123/trading-bot-2.0-pi:latest
